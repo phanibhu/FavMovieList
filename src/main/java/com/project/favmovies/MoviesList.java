ba5a0400ca28d361d7942b75449b77971324b04d
phani bhu <kallamariphanibhushan7@gmail.com>
2023-03-13 16:15:26 +0530

    Updated the code like if the fourth movie added then the first added movie will be removed

--- src/main/java/com/project/favmovies/MoviesList.java
+++ src/main/java/com/project/favmovies/MoviesList.java
@@ -1,18 +1,28 @@
 package com.project.favmovies;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class MoviesList {
     ArrayList<FavMovies> movie;
 
     public MoviesList(){
-        movie = new ArrayList<>(3);
+        movie = new ArrayList<>();
     }
 
 
-    public void addMovie(FavMovies favMovies){
-
+    public void addMovie(FavMovies favMovies) {
         movie.add(favMovies);
+    }
+    public void removeFirstMovie(FavMovies favMovies){
+
+
+        int value = 3;
+            if(movie.size()>=value){
+                movie.remove(0);
+                movie.add(favMovies);
+            }
+
 
     }
     // we can go with this method also with out initializing initial capacity
@@ -25,7 +35,7 @@ public class MoviesList {
 //            System.out.println("we can't enter fourth movie");
 //        }
 //
-//
+////
 //
 //    }
     public ArrayList<FavMovies> getMoviesList(){
--- src/test/java/com/project/MoviesListTest.java
+++ src/test/java/com/project/MoviesListTest.java
@@ -25,6 +25,23 @@ public class MoviesListTest {
         Assert.assertEquals(favMovies3, moviesList.getMoviesList().get(2));
     }
     @Test
+    public void removeFirstMovieFromList(){
+        MoviesList moviesList = new MoviesList();
+        FavMovies favMovies1 = new FavMovies("pushpa");
+        FavMovies favMovies2 = new FavMovies("ASVR");
+        FavMovies favMovies3 = new FavMovies("Jalsa");
+        FavMovies favMovies4 = new FavMovies("OG");
+        moviesList.addMovie(favMovies1);
+        moviesList.addMovie(favMovies2);
+        moviesList.addMovie(favMovies3);
+        moviesList.addMovie(favMovies4);
+        moviesList.removeFirstMovie(favMovies1);
+        Assert.assertEquals(4, moviesList.getMoviesList().size());
+        Assert.assertEquals(favMovies2, moviesList.getMoviesList().get(0));
+        Assert.assertEquals(favMovies3, moviesList.getMoviesList().get(1));
+        Assert.assertEquals(favMovies4, moviesList.getMoviesList().get(2));
+    }
+    @Test
     public void getFavMovieList2(){
         MoviesList moviesList = new MoviesList();
         FavMovies favMovies1 = new FavMovies("pushpa");
